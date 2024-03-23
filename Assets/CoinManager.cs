51a1dbf34cffb20a006908646ca8cbd79cd4a679
Jayabharathi3 <bharathibhuvana10@gmail.com>
2024-03-23 10:51:32 +0530

    Initial commit

--- Assets/CoinManager.cs
+++ Assets/CoinManager.cs
@@ -1,1 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinManager : MonoBehaviour
+{
+    public int coincount;
+    public Text value;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        value.text = coincount.ToString();
+    }
+}
 
